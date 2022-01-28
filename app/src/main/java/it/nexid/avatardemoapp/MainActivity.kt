3d02624f49a6e96afa5840c24b3bd1f1d18efed1
Rashid Amanzholov <r.amanzholov@nexid.it>
2022-01-28 18:53:12 +0100

    Implement WebView

--- app/src/main/java/it/nexid/avatardemoapp/MainActivity.kt
+++ app/src/main/java/it/nexid/avatardemoapp/MainActivity.kt
@@ -2,10 +2,24 @@ package it.nexid.avatardemoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myWebView: WebView = findViewById(R.id.webview)
+        myWebView.loadUrl("https://www.sitepal.com/api/examples/sayAudio.html")
+
+        val webSettings = myWebView.settings
+        webSettings.javaScriptEnabled = true
+
+        myWebView.webViewClient = WebViewClient()
+
+        myWebView.canGoBack()
+//        setContentView(myWebView)
+
     }
 }
